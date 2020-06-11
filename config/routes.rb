c9477dcff13944ae8a6055803acb1506a196bc6d
Hemant soni <sonihemant4741@gmail.com>
2020-06-11 22:22:54 +0530

    view for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "post#index"
+  root to: "posts#index"
 
-  resources :post, only:[:new, :index, :create]
+  resources :posts, only:[:new, :index, :create]
 end
